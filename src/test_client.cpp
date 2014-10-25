31caff9240871d79b2a311dcbbf8a4d11e4feb86
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2014-10-25 15:25:38 +0200

    Updated torso server, now works with amigo

--- src/test_client.cpp
+++ src/test_client.cpp
@@ -1,25 +1,32 @@
 #include <ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/terminal_state.h>
-#include <amigo_actions/AmigoSpindleCommandAction.h>
+#include <control_msgs/FollowJointTrajectoryAction.h>
 
 int main (int argc, char **argv) {
   ros::init(argc, argv, "test_client");
 
   if (argc != 2) {
-	  ROS_ERROR("Arguments: SPINDLE_HEIGHT");
+      ROS_ERROR("Arguments: SPINDLE_HEIGHT. Please note that this test client has been developed for AMIGO");
 	  return 1;
   }
 
   double spindle_height = atof(argv[1]);
 
   // create the action client
-  actionlib::SimpleActionClient<amigo_actions::AmigoSpindleCommandAction> ac("spindle_server");
+  //actionlib::SimpleActionClient<amigo_actions::AmigoSpindleCommandAction> ac("spindle_server");
+  actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> ac("torso_server");
   ac.waitForServer();
 
   // send a goal to the action
-  amigo_actions::AmigoSpindleCommandGoal goal;
-  goal.spindle_height = spindle_height;
+  control_msgs::FollowJointTrajectoryGoal goal;
+  goal.trajectory.header.stamp = ros::Time::now();
+  goal.trajectory.joint_names.resize(1);
+  goal.trajectory.joint_names[0] = "torso_joint";
+  goal.trajectory.points.resize(1);
+  goal.trajectory.points[0].positions.resize(1);
+  goal.trajectory.points[0].positions[0] = spindle_height;
+
 
   actionlib::SimpleClientGoalState state = ac.sendGoalAndWait(goal);
   ROS_WARN("Current state: %s", state.toString().c_str());
